b20f4336d6b069e4f6b626cbfaf4fbf725e75ce6
Darryl <hjdjoo@gmail.com>
2024-02-05 14:29:01 -0500

    Committing next-js file structure

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,10 @@ import { ServerError } from './types/server-types'
 const app = express();
 const PORT = 5000
 
+
+
 app.use(cors());
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`)
