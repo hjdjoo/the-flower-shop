7f5dcf507c0831eeac60fc3cadaaee1b925f55e8
Darryl <hjdjoo@gmail.com>
2024-02-27 11:26:52 -0500

    starting work on signup/auth protocols for admin/public access

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+// necessary components:
+
+// a dashboard that can control customer records, order records, order properties, delivery manager
 
 export default function Admin() {
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 console.log('Entering middleware.ts')
 
 export const middleware = async (res: NextResponse) => {
-
-
+  // return new NextResponse;
 }
 
 
 export const config = {
   matcher: [
-    '/'
+    '/auth'
   ]
 }
