c90c47ee864e3814bf4756bd8a5adfd71d29c198
Darryl <hjdjoo@gmail.com>
2024-06-11 16:10:27 -0400

    Trying some approaches to tighten up homepage appearance...

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,64 @@
-// "use client"
+"use client"
+
+import { useEffect, useState } from "react";
+
 import Products from "./products/page";
 import BackgroundBanner from "./_components/BackgroundBanner";
-import Head from "next/head";
+
 
 import getHomepageCategories from "@/utils/supabase/clientActions/getHomepageCategories";
 import { getBanners } from "@/utils/supabase/clientActions/getBanners";
 import { getUrls } from "@/utils/supabase/clientActions/getUrls";
+import { BannerData } from "./types/client-types";
+import Container from "@mui/material/Container";
 
 
-export default async function Main() {
+export default function Main() {
 
-  const { data: banners } = await getBanners();
+  const [bannerData, setBannerData] = useState<BannerData[]>([])
 
-  if (!banners) {
-    throw new Error("Couldn't get banners!")
-  }
+  useEffect(() => {
+    (async () => {
+      const { data: banners } = await getBanners();
 
-  let { data: bannerUrls } = await getUrls(banners, "banner_images");
+      if (!banners) {
+        throw new Error("Couldn't get banners!")
+      }
 
-  if (!bannerUrls) {
-    bannerUrls = [];
-  }
+      let { data: bannerUrls } = await getUrls(banners, "banner_images");
 
-  const bannerData = banners.map((banner, idx) => {
-    return {
-      name: banner,
-      url: bannerUrls[idx]
-    }
-  })
+      if (!bannerUrls) {
+        bannerUrls = [];
+      }
+
+      const data = banners.map((banner, idx) => {
+        return {
+          name: banner,
+          url: bannerUrls[idx]
+        }
+      })
+
+      setBannerData(data);
+    })()
+  }, [])
+
+  useEffect(() => {
+    console.log("navigator: ", navigator);
+    // document.querySelector()
+  }, [])
 
 
   return (
-    <>
+    <Container id="main-box"
+      disableGutters
+      sx={{
+        padding: "15px",
+        margin: 0,
+        // maxWidth: 
+      }}
+    >
       <BackgroundBanner bannerData={bannerData} />
       <Products />
-    </>
+    </Container>
   )
 }
